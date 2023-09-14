145a80836c3dbc68886c2b0b997a6520b5fc7453
FURKAN TURAL <furkanturalofficial@outlook.com>
2023-09-14 12:31:09 +0300

    Context implementation.

---
+++ Infrastructure/Archieves.Persistence/Contexts/ArchievesDbContext.cs
@@ -0,0 +1,22 @@
+﻿using KütüphaneOtomasyonu.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archieves.Persistence.Contexts
+{
+    public class ArchievesDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = FURKANTURAL; Database = ArchievesDb; Integrated Security = True");
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
