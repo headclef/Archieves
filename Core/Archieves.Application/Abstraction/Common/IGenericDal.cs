117a3c43b154b0d245455255dd3c5e0c0a65d007
FURKAN TURAL <furkanturalofficial@outlook.com>
2023-09-14 11:41:21 +0300

    Interfaces implementation.

---
+++ Core/Archieves.Application/Abstraction/Common/IGenericDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archieves.Application.Abstraction.Common
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        List<T> GetAll();
+        List<T> GetAll(Expression<Func<T, bool>> filter);
+        T GetById(int id);
+    }
+}
---
+++ Core/Archieves.Application/Abstraction/IBookDal.cs
@@ -0,0 +1,12 @@
+﻿using Archieves.Application.Abstraction.Common;
+using KütüphaneOtomasyonu.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archieves.Application.Abstraction
+{
+    public interface IBookDal : IGenericDal<Book> {}
+}
