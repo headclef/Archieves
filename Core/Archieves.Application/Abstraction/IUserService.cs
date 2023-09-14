cd2298ec900e1446ce322f1dfc75e882c41e5bf7
FURKAN TURAL <furkanturalofficial@outlook.com>
2023-09-14 11:47:40 +0300

    Services implementation.

---
+++ Core/Archieves.Application/Abstraction/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Archieves.Application.Abstraction.Common;
+using KütüphaneOtomasyonu.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archieves.Application.Abstraction
+{
+    public interface IUserService : IGenericService<User>
+    {
+        List<User> GetAll(int id);
+    }
+}
