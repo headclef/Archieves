93ff3c2d310a06c08f5933dbc91d2f63d64d7b2a
FURKAN TURAL <furkanturalofficial@outlook.com>
2023-09-18 17:11:03 +0300

    Presentation update.

--- Presentation/Archieves.Kutuphane/Controllers/RegisterController.cs
+++ Presentation/Archieves.Kutuphane/Controllers/RegisterController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Archieves.Kutuphane.ValidationRules;
+using Archieves.Persistence.Concretes;
+using FluentValidation.Results;
+using KütüphaneOtomasyonu.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KütüphaneOtomasyonu.Controllers
 {
 	public class RegisterController : Controller
 	{
+		UserManager userManager = new UserManager(new EfUserRepository());
+		[HttpGet]
 		public IActionResult Index()
 		{
-			return View();
+            return View();
+		}
+		[HttpPost]
+		public IActionResult Index(User user)
+		{
+			UserValidator uv = new UserValidator();
+			ValidationResult vr = uv.Validate(user);
+			if (!vr.IsValid)
+			{
+                foreach (var item in vr.Errors)
+				{
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }
+			else
+            {
+                user.Status = true;
+                user.Image = "empty";
+                userManager.Add(user);
+                return RedirectToAction("Index", "Login");
+            }
 		}
 	}
 }
---
+++ Presentation/Archieves.Kutuphane/ValidationRules/UserValidator.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using KütüphaneOtomasyonu.Domain.Entities;
+using System.Text.RegularExpressions;
+
+namespace Archieves.Kutuphane.ValidationRules
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("The name field can not be empty.");
+
+            RuleFor(x => x.Surname)
+                .NotEmpty()
+                .WithMessage("The surname field can not be empty.");
+            
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .WithMessage("The email field can not be empty and email must be valid.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(6)
+                .WithMessage("The Password can not be lesser than 6 characters.");
+
+            RuleFor(x => x.Phone)
+                .MaximumLength(11)
+                .WithMessage("The phone number can not be longer than 11 characters.");
+
+            RuleFor(x => x.Address)
+                .NotEmpty()
+                .MinimumLength(10)
+                .WithMessage("The address field can not be empty and address can not be lesser than 10 characters.");
+        }
+    }
+}
+/*
+ * .MustAsync(async (password, cancellation) =>
+                 {
+                     var regex = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$");
+                     return await Task.FromResult(regex.IsMatch(password));
+                 })
+*/
